a76ed4a3204c363f70d958657de69dc9de9348b1
Tanawat Tassana <amp.tanawat@gmail.com>
2012-10-22 19:47:49 +0700

    add routing /subscribers/create

--- raitong-mb/raitong-mb-web/src/main/webapp/angularjs/app.js
+++ raitong-mb/raitong-mb-web/src/main/webapp/angularjs/app.js
@@ -3,5 +3,6 @@ angular.module('raitong.mb', [])
 	.config(['$routeProvider', function($routeProvider) {
 		$routeProvider
 			.when('/subscribers', {templateUrl: 'resources/angularjs/partial/subscriber/subscriber-list.html'})
+			.when('/subscribers/create', {templateUrl: 'resources/angularjs/partial/subscriber/subscriber-form.html'})
 			.otherwise({redirectTo: '/subscribers'});
 	}]);
