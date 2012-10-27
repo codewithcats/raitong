274b4375b23940c9851c5469bef46c1e7c11a364
dranophoenix <dog_champ@hotmail.com>
2012-10-27 22:52:58 +0700

    add create subscription test cases

--- raitong-mb/raitong-mb-web/src/main/java/com/raitongorganicsfarm/app/mb/web/SubscriptionController.java
+++ raitong-mb/raitong-mb-web/src/main/java/com/raitongorganicsfarm/app/mb/web/SubscriptionController.java
@@ -1,6 +1,6 @@
 package com.raitongorganicsfarm.app.mb.web;
 
-import org.springframework.beans.factory.annotation.Required;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -9,44 +9,66 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.raitongorganicsfarm.app.mb.entity.Subscriber;
 import com.raitongorganicsfarm.app.mb.entity.Subscription;
+import com.raitongorganicsfarm.app.mb.repository.SubscriberRepository;
 import com.raitongorganicsfarm.app.mb.repository.SubscriptionRepository;
 import com.raitongorganicsfarm.app.mb.web.util.JsonUtil;
 import com.raitongorganicsfarm.app.mb.web.util.JsonUtilImpl;
 
 @Controller
 public class SubscriptionController {
-	
-	private SubscriptionRepository subscriptionRepository; 
-	private JsonUtil<Subscription> jsonUtil = new JsonUtilImpl<Subscription>();
 
-    @RequestMapping(method = RequestMethod.POST, value = "/subscriptions/")
-    public ResponseEntity<String> create(@RequestBody String body) {
-    	try {
-	    	Subscription subscription = jsonUtil.fromJson(body, Subscription.class);
-	    	subscription = this.subscriptionRepository.save(subscription);
-	    	return new ResponseEntity<String>(subscription.toJson(), HttpStatus.CREATED);
-    	} catch (RuntimeException r) {
-    		return new ResponseEntity<String>(HttpStatus.INTERNAL_SERVER_ERROR);
-    	}
-    }
-    
-    @RequestMapping(method = RequestMethod.GET, value = "/subscriptions/{id}")
-    public ResponseEntity<String> info(@PathVariable String id) {
-    	try {
-	    	Subscription subscription = this.subscriptionRepository.findOne(id);
-	    	return new ResponseEntity<String>(subscription.toJson(), HttpStatus.OK);
-    	} catch (RuntimeException r) {
-    		return new ResponseEntity<String>(HttpStatus.INTERNAL_SERVER_ERROR);
-    	}
-    }
-    
-    @Required
-    public void setSubscriptionRepository(SubscriptionRepository subscriptionRepository) {
+	private SubscriptionRepository subscriptionRepository;
+	private SubscriberRepository subscriberRepository;
+	private JsonUtil<Subscription> jsonUtil = new JsonUtilImpl<Subscription>();
+	private Subscriber subscriber;
+
+	@RequestMapping(method = RequestMethod.POST, value = "/subscriber/{customerNo}/subscriptions/")
+	public ResponseEntity<String> create(@PathVariable String customerNo, @RequestBody String body) {
+		if(body == null) {
+			return new ResponseEntity<String>(HttpStatus.BAD_REQUEST);
+		}
+		try {
+			subscriber = subscriberRepository.findByCustomerNo(customerNo);
+			if(subscriber == null) {
+				return new ResponseEntity<String>(HttpStatus.BAD_REQUEST);
+			}
+			Subscription subscription = jsonUtil.fromJson(body, Subscription.class);
+			subscription = this.subscriptionRepository.save(subscription);
+			
+			
+			return new ResponseEntity<String>(subscription.toJson(), HttpStatus.CREATED);
+		} catch (RuntimeException r) {
+			return new ResponseEntity<String>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+
+	@RequestMapping(method = RequestMethod.GET, value = "/subscriptions/{id}")
+	public ResponseEntity<String> info(@PathVariable String id) {
+		try {
+			Subscription subscription = this.subscriptionRepository.findOne(id);
+			return new ResponseEntity<String>(subscription.toJson(), HttpStatus.OK);
+		} catch (RuntimeException r) {
+			return new ResponseEntity<String>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+
+	@Autowired
+	public void setSubscriptionRepository(SubscriptionRepository subscriptionRepository) {
 		this.subscriptionRepository = subscriptionRepository;
 	}
-    
-    public void setJsonUtil(JsonUtil<Subscription> jsonUtil) {
+
+	@Autowired
+	public void setSubscriberRepository(SubscriberRepository subscriberRepository) {
+		this.subscriberRepository = subscriberRepository;
+	}
+
+	public void setJsonUtil(JsonUtil<Subscription> jsonUtil) {
 		this.jsonUtil = jsonUtil;
 	}
+	
+	public Subscriber getSubscriber() {
+	    return subscriber;
+    }
 }
--- raitong-mb/raitong-mb-web/src/test/java/com/raitongorganicsfarm/app/mb/web/SubscriptionController_UnitTest.java
+++ raitong-mb/raitong-mb-web/src/test/java/com/raitongorganicsfarm/app/mb/web/SubscriptionController_UnitTest.java
@@ -4,6 +4,7 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 import static org.junit.Assert.*;
 
+import java.util.LinkedList;
 import java.util.Random;
 
 import org.junit.Before;
@@ -12,7 +13,9 @@ import org.junit.Test;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+import com.raitongorganicsfarm.app.mb.entity.Subscriber;
 import com.raitongorganicsfarm.app.mb.entity.Subscription;
+import com.raitongorganicsfarm.app.mb.repository.SubscriberRepository;
 import com.raitongorganicsfarm.app.mb.repository.SubscriptionRepository;
 import com.raitongorganicsfarm.app.mb.web.util.JsonUtil;
 
@@ -26,14 +29,15 @@ public class SubscriptionController_UnitTest {
 	}
 
 	@Test
-	public void testCreateSuccess() {
+	public void testCreateNewSubscriptionSuccess() {
+		// mock subsription
 		Subscription s = new Subscription();
 		String reqBody = s.toString();
 
 		Subscription expected = new Subscription();
-		String id = String.valueOf(new Random().nextLong());
+		String id = generateLongStringId();
 		expected.setId(id);
-		
+
 		JsonUtil<Subscription> jsonUtil = mock(JsonUtil.class);
 		when(jsonUtil.fromJson(reqBody, Subscription.class)).thenReturn(s);
 		this.subscriptionController.setJsonUtil(jsonUtil);
@@ -41,51 +45,96 @@ public class SubscriptionController_UnitTest {
 		SubscriptionRepository repo = mock(SubscriptionRepository.class);
 		when(repo.save(s)).thenReturn(expected);
 		this.subscriptionController.setSubscriptionRepository(repo);
-		
-		ResponseEntity<String> actual = this.subscriptionController.create(reqBody);
+
+		Subscriber subscriber = prepareMockingCompleteSubscriberWithBlankSubscriptions(this.subscriptionController);
+		String customerNo = subscriber.getCustomerNo();
+		// test
+		ResponseEntity<String> actual = this.subscriptionController.create(customerNo, reqBody);
 		assertEquals(HttpStatus.CREATED, actual.getStatusCode());
 		assertEquals(expected.toJson(), actual.getBody());
+		Subscriber actualSubscriber = this.subscriptionController.getSubscriber();
+		assertNotNull(actualSubscriber);
+		assertEquals(subscriber, actualSubscriber);
 	}
-	
+
+	@Test
+	public void testCreateSubscriptionButNotFoundSubscriber() {
+		// mock subscriber
+		SubscriberRepository subscriberRepository = mock(SubscriberRepository.class);
+		String customerNo = generateLongStringId();
+		when(subscriberRepository.findByCustomerNo(customerNo)).thenReturn(null);
+		this.subscriptionController.setSubscriberRepository(subscriberRepository);
+
+		Subscription subscription = new Subscription();
+		String body = subscription.toJson();
+		ResponseEntity<String> req = this.subscriptionController.create(customerNo, body);
+		assertEquals(HttpStatus.BAD_REQUEST, req.getStatusCode());
+	}
+
+	@Test
+	public void testCreateNullSubscription() {
+		Subscriber subscriber = prepareMockingCompleteSubscriberWithBlankSubscriptions(this.subscriptionController);
+
+		ResponseEntity<String> req = subscriptionController.create(subscriber.getCustomerNo(), null);
+		assertEquals(HttpStatus.BAD_REQUEST, req.getStatusCode());
+	}
+
+	private Subscriber prepareMockingCompleteSubscriberWithBlankSubscriptions(SubscriptionController controller) {
+		Subscriber subscriber = new Subscriber();
+		subscriber.setId(generateLongStringId());
+		subscriber.setCustomerNo(generateLongStringId());
+		subscriber.setSubscriptions(new LinkedList<Subscription>());
+		String customerNo = subscriber.getCustomerNo();
+
+		SubscriberRepository subscriberRepository = mock(SubscriberRepository.class);
+		when(subscriberRepository.findByCustomerNo(customerNo)).thenReturn(subscriber);
+		when(subscriberRepository.save(subscriber)).thenReturn(subscriber);
+		controller.setSubscriberRepository(subscriberRepository);
+		return subscriber;
+	}
+
+	private String generateLongStringId() {
+		return String.valueOf(new Random().nextLong());
+	}
+
 	@Test
 	public void testCreateRuntimeException() {
 		SubscriptionRepository repo = mock(SubscriptionRepository.class);
 		Subscription s = new Subscription();
 		when(repo.save(s)).thenThrow(new RuntimeException());
 		this.subscriptionController.setSubscriptionRepository(repo);
-		
+
 		String reqBody = s.toJson();
-		
+
 		JsonUtil<Subscription> jsonUtil = mock(JsonUtil.class);
 		when(jsonUtil.fromJson(reqBody, Subscription.class)).thenReturn(s);
 		this.subscriptionController.setJsonUtil(jsonUtil);
-		
-		ResponseEntity<String> req = this.subscriptionController.create(reqBody);
+
+		ResponseEntity<String> req = this.subscriptionController.create("error", reqBody);
 		assertEquals(HttpStatus.INTERNAL_SERVER_ERROR, req.getStatusCode());
 	}
-	
-	
+
 	@Test
 	public void testInfoSuccess() {
 		SubscriptionRepository repo = mock(SubscriptionRepository.class);
-		String id = "12323";
+		String id = generateLongStringId();
 		Subscription expected = new Subscription();
 		expected.setId(id);
 		when(repo.findOne(id)).thenReturn(expected);
 		this.subscriptionController.setSubscriptionRepository(repo);
-		
+
 		ResponseEntity<String> req = this.subscriptionController.info(id);
 		assertEquals(HttpStatus.OK, req.getStatusCode());
 		assertEquals(id, Subscription.fromJsonToSubscription(req.getBody()).getId());
 	}
-	
+
 	@Test
 	public void testInfoRuntimeException() {
 		SubscriptionRepository repo = mock(SubscriptionRepository.class);
-		String id = "exception";
+		String id = generateLongStringId();
 		when(repo.findOne(id)).thenThrow(new RuntimeException());
 		this.subscriptionController.setSubscriptionRepository(repo);
-		
+
 		ResponseEntity<String> req = this.subscriptionController.info(id);
 		assertEquals(HttpStatus.INTERNAL_SERVER_ERROR, req.getStatusCode());
 	}
