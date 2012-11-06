6d4ad26b75740cd436890e429adfd048050c8bea
Tanawat Tassana <amp.tanawat@gmail.com>
2012-11-06 11:43:47 +0700

    move CreateSubscriberCtrl controller declaration to app.js

--- raitong-mb/raitong-mb-web/src/main/webapp/angularjs/app.js
+++ raitong-mb/raitong-mb-web/src/main/webapp/angularjs/app.js
@@ -3,7 +3,7 @@ angular.module('raitong.mb', ['raitong.mb.directive', 'raitong.mb.service'])
 	.config(['$routeProvider', function($routeProvider) {
 		$routeProvider
 			.when('/subscribers', {templateUrl: 'resources/angularjs/partial/subscriber/subscriber-list.html'})
-			.when('/subscribers/create', {templateUrl: 'resources/angularjs/partial/subscriber/subscriber-form.html'})
+			.when('/subscribers/create', {templateUrl: 'resources/angularjs/partial/subscriber/subscriber-form.html', controller: 'CreateSubscriberCtrl'})
 			.when('/subscribers/:customerNo', {templateUrl: 'resources/angularjs/partial/subscriber/subscriber-info.html'})
       .otherwise({redirectTo: '/subscribers'});
 	}]);
